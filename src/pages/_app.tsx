21e556bfa6318c8ccc22fb0177072ee44adcf76b
AR. Arif <dev.ar.arif@gmail.com>
2022-12-02 20:02:35 +0600

    Added meta tag for API request approval

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.scss';
 import '../../build/styles/tailwind.css';
+
 import type { AppProps } from 'next/app';
-import MainLayout from '../layouts/MainLayout';
 import Context from '../context/Context';
-import { useEffect } from 'react';
-import { gsap } from 'gsap';
+import Head from 'next/head';
+import MainLayout from '../layouts/MainLayout';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { gsap } from 'gsap';
+import { useEffect } from 'react';
+
 /**
  * Main app component for the application
  *
@@ -22,6 +25,9 @@ export default function App({ Component, pageProps }: AppProps) {
 
     return (
         <>
+            <Head>
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+            </Head>
             <Context>
                 <MainLayout>
                     <Component {...pageProps} />
